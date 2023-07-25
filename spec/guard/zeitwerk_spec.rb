f411c5cfc3c482ca03e9162f257a5f4178dc00a8
Brad Gessler <bradgessler@gmail.com>
2023-07-24 17:02:29 -0700

    Delete failing spec

--- spec/guard/zeitwerk_spec.rb
+++ spec/guard/zeitwerk_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe Guard::Zeitwerk do
   it "has a version number" do
-    expect(Guard::Zeitwerk::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(Guard::ZeitwerkGuard::VERSION).not_to be nil
   end
 end
